17de7e1cc2d85b604291ba7851ba6f356d9c3ed1
Leak <sethpowellbusiness@gmail.com>
2021-08-11 17:04:56 -0700

    Cleaned up remaining code like removing console.logs and reworked the other statements printed to console

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+console.log("Welcome to the roulette table, you can bet on red, black, or any number between 0 and 36. You currently have $100 worth of chips and it takes $10 in chips to play. Winning a bet on the color will net you a jackpot up to $35. Winning a bet on the number will net you a jackpot $105")
+
 let wallet= 100
 
 priceToPlay= 10
 
 rouletteWheel = [
     [0,'green'],
-    [00,'green'],
     [1,'red'],
     [2,'black'],
     [3,'red'],
@@ -43,29 +44,25 @@ rouletteWheel = [
     [36,'red']
 ]
 
-jackpot = (priceToPlay*(Math.random()*3).toPrecision(2))
+jackpot = ((priceToPlay+1)*(Math.random()*3).toPrecision(2))
 
 function spinTheWheel() {
     randomNum = (Math.floor(Math.random()*rouletteWheel.length))
-    console.log((rouletteWheel[randomNum])[0].toString())
     return (rouletteWheel[randomNum])
 }
-// create a loop so this runs until your wallet reaches 0 and you bust
+
 function playRoulette() {
   if (wallet >= 10) {
     bet = prompt('What would you like to bet on?')
-    console.log(bet)
     if (isNaN(bet) === false) {
-      console.log('number')
       spinTheWheel()
       betNumber(bet)
     } else if (bet.toString() === 'red' || 'black') {
-      console.log('color')
       spinTheWheel();
       betColor(bet);
     }
   } else {
-    console.log("Sorry you're too poor to play this game")
+    console.log("Sorry you're too poor to play this game, security will show you out. . . . . ya loser")
   }
 }
 
@@ -73,12 +70,12 @@ function betColor(bet) {
   if (bet === 'red' || 'black') {
     if ((rouletteWheel[randomNum])[1] === bet) {
         wallet += jackpot
-        console.log(`How does it feel to be a winner? Well now you know because the spin came up ${bet}`)
+        console.log(`Congratulations the spin came up ${bet}`)
         console.log(`You now have $${wallet.toPrecision(5)} why not play again.`)
         playRoulette()
     } else {
         wallet -= priceToPlay
-        console.log(`How does it feel to be a loser? The spin unfortuanately was not ${bet}, but you might win back what you lost on the next spin!!!`)
+        console.log(`Sorry, the spin was not ${bet}, but you could win back what you lost on the next spin!!!`)
         console.log(`You now have $${wallet.toPrecision(5)} why not play again.`)
         playRoulette()
     }
@@ -87,13 +84,13 @@ function betColor(bet) {
 
 function betNumber(bet) {
   if (bet.toString() === ((rouletteWheel[randomNum])[0]).toString()) {
-      wallet += jackpot
-      console.log(`How does it feel to be a winner? Well now you know because the spin came up ${bet}`)
+      wallet += (jackpot*3)
+      console.log(`Congratulations the spin came up ${bet}`)
       console.log(`You now have $${wallet.toPrecision(5)} why not play again.`)
       playRoulette()
   } else {
       wallet -= priceToPlay
-      console.log(`How does it feel to be a loser? The spin unfortuanately was not ${bet}, but you might win back what you lost on the next spin!!!`)
+      console.log(`Sorry, the spin was not ${bet}, but you could win back what you lost on the next spin!!!`)
       console.log(`You now have $${wallet.toPrecision(5)} why not play again.`)
       playRoulette()
   }
